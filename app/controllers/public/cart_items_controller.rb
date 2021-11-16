a2d0453fa19e547c6e1d2a050c2facca800a68e8
yu-M215 <yuu0609km.215@gmail.com>
2021-11-16 09:25:22 +0000

    Update cart_item_controller.rb

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,16 +1,36 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = CartItems.where(customer_id: current_customer.id)
+    @items = Products.find(@cart_items.product_id)
+    @total_price = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
   end
   
   def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    @cart_item.save
+    redirect_to public_cart_items_path
   end
   
   def update
+    @item = CartItem.find(params[:id])
+    @item.update
+    redirect_back(fallback_location: public_cart_items_path)
   end
   
   def destroy
+    @item = CartItem.find_by(params[:id]).destroy
+    redirect_back(fallback_location: public_cart_items_path)
   end
   
   def destroy_all
+    CartItem.where(customer_id: :cuurent_customer.id).destroy_all
+    redirect_back(fallback_location: public_cart_items_path)
+  end
+  
+  private
+  
+  def cart_item_params
+    params.require(:cart_item).permit(:quanitity)
   end
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,8 @@
 class CartItem < ApplicationRecord
+    
+    
+  # カート内商品の小計算出
+  def sum_of_price
+    product.tax_in_price * quanitity
+  end
 end
