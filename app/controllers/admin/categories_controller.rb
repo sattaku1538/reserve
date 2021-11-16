b01c8c4b1f3d82932fc7bae97141a72bb9d2168b
usgreenfox <usgreenfox@gmail.com>
2021-11-16 10:58:55 +0000

    Add orders,products,categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,13 +1,37 @@
 class Admin::CategoriesController < ApplicationController
+  before_action :set_category, only: %i[edit update]
+  
   def index
+    @categories = Category.all
+    @category = Category.new
   end
   
   def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to admin_categories_path
+    else
+      render 'index'
+    end
   end
 
   def edit
   end
   
   def update
+    if @category.update(category_params)
+      redirect_to admin_categories_path
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
+  
+  def set_category
+    @category = Category.find(params[:id])
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,11 +1,21 @@
 class Admin::ProductsController < ApplicationController
+  before_action :set_product, only: %i[show edit update]
+  
   def index
+    @products = Product.all
   end
 
   def new
+    @product = Product.new
   end
   
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_products_path
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -15,5 +25,19 @@ class Admin::ProductsController < ApplicationController
   end
   
   def update
+    if @product.update(product_params)
+      redirect_to admin_products_path
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+  def product_params
+    params.require(:product).permit(:name, :discription, :image, :category, :price, :is_deleted)
+  end
+  
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
