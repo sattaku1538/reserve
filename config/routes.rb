8cf6afbfd9b8c8459ee589795bf858d04eefb013
usgreenfox <usgreenfox@gmail.com>
2021-11-16 06:57:40 +0000

    Add admin/orders_controller with views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :admin do
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :admin do
     get 'products/index'
     get 'products/new'
     get 'products/show'
