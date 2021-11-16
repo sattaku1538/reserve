4071813c89ee44054acd869da3353802a5882667
yu-M215 <yuu0609km.215@gmail.com>
2021-11-16 07:46:20 +0000

    Update product.rb, category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  # アソシエーション
+  has_many :products
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+  # アソシエーション
+  has_many :cart_items, dependent: :destroy
+  has_many :order_details, dependent: :destroy
+
+  belongs_to :category
+  
+  # refileを使用して商品画像をアップする
+  attachment :image
+  
 end
