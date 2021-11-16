045c88984f46886b8b52eb46c2e7e06519326084
yu-M215 <yuu0609km.215@gmail.com>
2021-11-16 08:28:29 +0000

    Update products_controller.rb

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,7 +1,11 @@
 class Public::ProductsController < ApplicationController
   def index
+    @products = Products.where(is_sold: true)
+    @categories = Categories.all
   end
 
   def show
+    @product = Product.find(params[:id])
+    @categories = Category.all
   end
 end
