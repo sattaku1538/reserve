8ea223bb1a3c898fa075479af907af3416e29960
yu-M215 <yuu0609km.215@gmail.com>
2021-11-16 13:23:42 +0000

    Fix and Update order_controller.rb

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,15 +3,14 @@ class Public::CartItemsController < ApplicationController
   before_action :set_item
   
   def index
-    @cart_items = CartItems.where(customer_id: current_customer.id)
+    @cart_items = current_customer.cart_items.all
     @items = Products.find(@cart_items.product_id)
     # カート内商品の合計金額を算出
     @total_price = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
   end
   
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
+    @cart_item = current_customer.cart_items.new(cart_item_params)
     @cart_item.save
     redirect_to public_cart_items_path
   end
@@ -27,14 +26,14 @@ class Public::CartItemsController < ApplicationController
   end
   
   def destroy_all
-    CartItem.where(customer_id: :cuurent_customer.id).destroy_all
+    current_customer.cart_items.destroy_all
     redirect_back(fallback_location: public_cart_items_path)
   end
   
   private
   
   def cart_item_params
-    params.require(:cart_item).permit(:quanitity)
+    params.require(:cart_item).permit(:product_id,:quanitity)
   end
   
   def set_item
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,20 +1,49 @@
 class Public::OrdersController < ApplicationController
+  
+  before_action :set_item_from_cart only: [:confirm,:create]
 
   def index
+    @orders = Order.all
   end
 
   def show
+    @order = Order.find(params[:id])
   end
 
   def new
+    @order = Order.new
+    @addresses = current_customer.shippings.all
   end
 
   def confirm
+    if select_address == 0
+      @order.post_code = current_customer.post_code
+      @order.address = current_customer.address
+      @order.name = current_customer.first_name + current_customer.last_name
+    elsif select_address == 1
+      @address = Shipping.find(params[:order][:address_id])
+      @order.post_code = @address.post_code
+      @order.address = @address.address
+      @order.name = @address.name
+    elsif select_address == 2
+      @order = Order.new(order_params)
+    end
   end
   
   def create
+    @order = current_customer.orders.new(order_params)
+    @order.save
+    @order_items.save
+    redirect_to public_orders_complete_path
   end
   
   def complete
   end
+  
+  private
+  
+  def order_params
+      params.require(:order).permit(:post_code, :address, :address_name, :payment_method)
+  end
+
 end
