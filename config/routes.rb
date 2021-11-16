0086c318435bcd09f63da6f9033299668e00c4b9
usgreenfox <usgreenfox@gmail.com>
2021-11-16 06:55:33 +0000

    Add admin/products_controller with views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'products/index'
+    get 'products/new'
+    get 'products/show'
+    get 'products/edit'
+  end
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
