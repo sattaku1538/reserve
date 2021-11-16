02c342d42fb375c52d10d78bb3b76fc36d0591d0
yu-M215 <yuu0609km.215@gmail.com>
2021-11-16 13:47:23 +0000

    Update orders_controller.rb

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,9 +1,6 @@
 class Public::OrdersController < ApplicationController
-  
-  before_action :set_item_from_cart only: [:confirm,:create]
-
   def index
-    @orders = Order.all
+    @orders = current_customer.orders.all
   end
 
   def show
@@ -16,6 +13,8 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @cart_items = current_user.cart_items.all
+    # 住所選択で分岐(0:メインの住所,1:登録済みの住所,2:新規住所) 
     if select_address == 0
       @order.post_code = current_customer.post_code
       @order.address = current_customer.address
@@ -31,9 +30,19 @@ class Public::OrdersController < ApplicationController
   end
   
   def create
+    # 入力された内容をorderに保存
     @order = current_customer.orders.new(order_params)
     @order.save
-    @order_items.save
+    # カートの商品をorder_detailに保存
+    @cart_items = current_user.cart_items.all
+    @cart_items.each do |cart_item|
+      @order_item = @order.order_details.new
+      @order_item.product_id = cart_item.product_id
+      @order_item.price = cart_item.product.tax_in_price
+      @order_item.quantity = cart_item.quantity
+      @order_item.save
+    end    
+    
     redirect_to public_orders_complete_path
   end
   
