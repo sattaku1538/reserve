19c14c51525ee8a04d13c035159d9864cdb9dbb2
EC2 Default User <ec2-user@ip-172-31-40-46.ec2.internal>
2021-11-16 06:56:55 +0000

    Add Controller(Products,Cart_items,Orders)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'orders/new'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'products/index'
+    get 'products/show'
+  end
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
