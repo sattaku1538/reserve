bd2c7b578e77e050f8002ceec821fe8d62a63765
usgreenfox <usgreenfox@gmail.com>
2021-11-16 12:35:25 +0000

    Add methods to public/shippings_controller

---
+++ app/controllers/public/shippings_controller.rb
@@ -0,0 +1,42 @@
+class Public::ShippingsController < ApplicationController
+  before_action :set_shipping, only: %i[edit update destroy]
+  def index
+    @shippings = current_customer.shippings.all
+    @shipping = Shipping.new
+  end
+  
+  def create
+    @shipping = current_customer.shippings.new(shipping_params)
+    if @shipping.save
+      redirect_to public_shippings_path
+    else
+      @shippings = current_customer.shippings.all
+      render 'index'
+    end
+  end
+
+  def edit
+  end
+  
+  def update
+    if @shipping.update(shipping_params)
+      redirect_to public_shippings_path
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @shipping.destroy
+    redirect_to public_shippings_path
+  end
+  
+  private
+  def set_shipping
+    @shipping = Shipping.find(params[:id])
+  end
+  
+  def shipping_params
+    params.require(:shipping).permit(:post_code, :address, :address_name)
+  end
+end
---
+++ app/helpers/public/shippings_helper.rb
@@ -0,0 +1,2 @@
+module Public::ShippingHelper
+end
