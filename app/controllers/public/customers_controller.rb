73addeb05b1c9d2b8c9e63844f01409fa0b4720a
sattaku1538 <choose.chose.chosen@gmail.com>
2021-11-17 05:46:20 +0000

    update controller customer

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -3,15 +3,21 @@ class Public::CustomersController < ApplicationController
     # before_action :authenticate_customer, except: [:？？？？？？？？]
 
     def show
-    
+     @customer = current_customer
 	end
 
 	def edit
-    
+	 @customer = current_customer	
 	end
 	
 	def update
-    
+	 @customer = current_customer
+	  if @customer.update(customer_params)
+	  	redirect_to customer_path
+	  	# フラッシュメッセージいれる？notice: "You have updated your account successfully."
+	    else
+	     render "edit"
+      end
     end
 	
 	def unsubscribe
