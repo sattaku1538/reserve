0b0e00d1279d695a57768e24ef57b0e28c0633ec
KOKIGOURMET <cuisinekoki@gmail.com>
2021-11-17 05:01:02 +0000

    Update controller admin_customer and Add public customer contoller

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,32 +1,35 @@
 class Public::CustomersController < ApplicationController
-    
+
     # before_action :authenticate_customer, except: [:？？？？？？？？]
 
     def show
-    
+
 	end
 
 	def edit
-    
-	end
-	
-	def update
-    
-    end
-	
-	def unsubscribe
-	   # なかじさん　Viewページの作成をお願いします。？
-	end
-	
-	def withdraw
-	   # なかじさん
+
 	end
 
+	def update
+
+    end
+
+	   # なかじさん　Viewページの作成をお願いします。？
+	def unsubscribe
+    	@customer = Customer.find_by(name: params[:name])
+	end
+
+	def withdraw
+    	@customer = Customer.find_by(name: params[:name])
+    	@customer.update(is_valid: false)
+    	reset_session
+    	redirect_to root_path
+	end
 
 	private
 
 # 	def params
-  	
-    end
-    
+
+#    end
+
 end
--- app/controllers/public:customer/sessions_controller.rb
+++ app/controllers/public:customer/sessions_controller.rb
@@ -2,7 +2,16 @@
 
 class Public:customer::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+  before_action :reject_inacrive_customer, only: [:create]
+  
+  def reject_inactive_customer
+    @customer = Customer.find_by(name: params[:customer] [:name])
+    if @customer
+      if @customer.valid_password?(params[:customer][:password]) && !@user.is_valid
+        redirect_to new_user_session_path
+      end
+    end
+  end
   # GET /resource/sign_in
   # def new
   #   super
