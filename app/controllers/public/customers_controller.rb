3832edc875236c40b3e1baaaa597004e57be492f
sattaku1538 <choose.chose.chosen@gmail.com>
2021-11-17 08:37:28 +0000

    Fix false

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -25,7 +25,7 @@ class Public::CustomersController < ApplicationController
     end
 
 	def withdraw
-    	@customer.update(is_deleted: false)
+    	@customer.update(is_deleted: true)
     	reset_session
     	redirect_to root_path
 	end
