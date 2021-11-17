38510d4e43d9fe1c0d8b6aa4696f967d31638b86
usgreenfox <usgreenfox@gmail.com>
2021-11-17 07:09:00 +0000

    Add view to public/top

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 現状シード作れない（image_idが必要？）
+# Product.create(name: 'cake1', discription: 'good', category_id: 1, is_sold: true, price: 500)
+# Product.create(name: 'cake2', discription: 'good', category_id: 2, is_sold: true, price: 500)
+# Product.create(name: 'cake3', discription: 'good', category_id: 3, is_sold: true, price: 500)
+# Product.create(name: 'cake4', discription: 'good', category_id: 4, is_sold: true, price: 500)
+
+Category.create(name: 'cake')
+Category.create(name: 'baked')
+Category.create(name: 'pudding')
+Category.create(name: 'candy')
