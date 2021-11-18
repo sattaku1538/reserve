5a685bb97f34c3c10027284cdb0f790dde5036fc
usgreenfox <usgreenfox@gmail.com>
2021-11-18 08:34:33 +0000

    Fix admin/orders for enum

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,6 @@
 class OrderDetail < ApplicationRecord
   belongs_to :product
   belongs_to :order
+  
+  enum making_status: { default: 0, ready: 1, making: 2, finish: 3 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -20,11 +20,58 @@ customers = Customer.create!([
     telephone_number: '09012345678',
     is_deleted: false,
   }])
-categories = Category.create!([{ name: 'ケーキ' }, { name: '焼き菓子' }, { name: 'プリン' }, { name: 'キャンディ' }])
 
-Product.create!(name: 'cake1', discription: 'good', image: File.open("./app/assets/images/cake.jpeg"), category_id: categories.first.id, is_sold: true, price: 500)
-Product.create!(name: 'cake2', discription: 'good', image: File.open("./app/assets/images/cake.jpeg"), category_id: categories.first.id, is_sold: true, price: 500)
-Product.create!(name: 'cake3', discription: 'good', image: File.open("./app/assets/images/cake.jpeg"), category_id: categories.last.id, is_sold: true, price: 500)
-Product.create!(name: 'cake4', discription: 'good', image: File.open("./app/assets/images/cake.jpeg"), category_id: categories.last.id, is_sold: true, price: 500)
+categories = Category.create!([
+  { name: 'ケーキ' }, 
+  { name: '焼き菓子' }, 
+  { name: 'プリン' }, 
+  { name: 'キャンディ' }
+  ])
 
-Order.create!(customer_id: customers.first.id, post_code: "5551234", address: "大阪", address_name: "山田花子", postage: 800, total_price: 2000, payment_method: 1, status: 1)
+products = Product.create!([
+  {
+    name: 'cake1', 
+    discription: 'good', 
+    image: File.open("./app/assets/images/cake.jpeg"), 
+    category_id: categories.first.id, 
+    is_sold: true, 
+    price: 500
+  },
+  {
+    name: 'cake2', 
+    discription: 'good', 
+    image: File.open("./app/assets/images/cake.jpeg"), 
+    category_id: categories.first.id, 
+    is_sold: true, 
+    price: 500
+  },
+  {
+    name: 'cake3',
+    discription: 'good', 
+    image: File.open("./app/assets/images/cake.jpeg"), 
+    category_id: categories.last.id, 
+    is_sold: true,
+    price: 500
+  },
+  {
+    name: 'cake4', 
+    discription: 'good', 
+    image: File.open("./app/assets/images/cake.jpeg"), 
+    category_id: categories.last.id, 
+    is_sold: true, 
+    price: 500
+  }])
+
+orders = Order.create!([
+  {
+    customer_id: customers.first.id,
+    post_code: "5551234", 
+    address: "大阪",
+    address_name: "山田花子", 
+    postage: 800, 
+    total_price: 2000, 
+    payment_method: 1, 
+    status: 1
+  }])
+  
+OrderDetail.create!(order_id: orders.first.id, product_id: products.first.id, quantity: 3, price: 1500, making_status: 0)
