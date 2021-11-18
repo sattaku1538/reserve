d29e00a1e8163fab500002432de95fe57083ef70
usgreenfox <usgreenfox@gmail.com>
2021-11-18 04:38:55 +0000

    Fix seeds.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
 
-  belongs_to :category, optional: true
+  belongs_to :category
   
   # refileを使用して商品画像をアップする
   attachment :image
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+#config: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,13 +7,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# 現状シード作れない（image_idが必要？）
-Product.create(name: 'cake1', discription: 'good', image: File.open("./app/assets/images/cake.jpeg"), category_id: 1, is_sold: true, price: 500)
-Product.create(name: 'cake2', discription: 'good', image: File.open("./app/assets/images/cake.jpeg"), category_id: 2, is_sold: true, price: 500)
-Product.create(name: 'cake3', discription: 'good', image: File.open("./app/assets/images/cake.jpeg"), category_id: 3, is_sold: true, price: 500)
-Product.create(name: 'cake4', discription: 'good', image: File.open("./app/assets/images/cake.jpeg"), category_id: 4, is_sold: true, price: 500)
 
-Category.create(name: 'ケーキ')
-Category.create(name: '焼き菓子')
-Category.create(name: 'プリン')
-Category.create(name: 'キャンディ')
+categories = Category.create!([{ name: 'ケーキ' }, { name: '焼き菓子' }, { name: 'プリン' }, { name: 'キャンディ' }])
+
+Product.create!(name: 'cake1', discription: 'good', image: File.open("./app/assets/images/cake.jpeg"), category_id: categories.first.id, is_sold: true, price: 500)
+Product.create!(name: 'cake2', discription: 'good', image: File.open("./app/assets/images/cake.jpeg"), category_id: categories.first.id, is_sold: true, price: 500)
+Product.create!(name: 'cake3', discription: 'good', image: File.open("./app/assets/images/cake.jpeg"), category_id: categories.last.id, is_sold: true, price: 500)
+Product.create!(name: 'cake4', discription: 'good', image: File.open("./app/assets/images/cake.jpeg"), category_id: categories.last.id, is_sold: true, price: 500)
