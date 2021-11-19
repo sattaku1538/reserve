ed3dc1ce06d16f8abe06bdfa54e0f9f7bf02f3ce
yu-M215 <yuu0609km.215@gmail.com>
2021-11-19 08:43:37 +0000

    Add action when customer is not signed in

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,6 +1,7 @@
 class Public::CartItemsController < ApplicationController
 
   before_action :set_item, only:[:update,:destroy]
+  before_action :signed_in?
 
   def index
     @cart_items = current_customer.cart_items.all
@@ -49,5 +50,12 @@ class Public::CartItemsController < ApplicationController
 
   def set_item
     @item = CartItem.find(params[:id])
+  end
+
+  def signed_in?
+    unless customer_signed_in?
+      # flash[:notice] = "ログインしてください"
+      redirect_to public_root_path
+    end
   end
 end
