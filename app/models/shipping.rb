bdf9bf3f0419fc82ef11be8db4a148c93e5682f4
sattaku1538 <choose.chose.chosen@gmail.com>
2021-11-20 01:49:27 +0000

    update models order_datails and orders

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,3 +1,11 @@
 class Shipping < ApplicationRecord
-    
+    belongs_to :customer
+
+	validates :customer_id, :name, :address, presence: true
+	validates :post_code, length: {is: 7}, numericality: { only_integer: true }
+
+	# order/newで使用
+# 	def order_address
+# 			self.post_code + self.address + self.name
+# 	end
 end
