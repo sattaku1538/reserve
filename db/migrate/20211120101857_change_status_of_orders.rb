efec917459df056a10e0d9f5f467a38a1383d1ea
yu-M215 <yuu0609km.215@gmail.com>
2021-11-21 08:16:26 +0000

    delete migration file

--- db/migrate/20211120101857_change_status_of_orders.rb
+++
@@ -1,5 +0,0 @@
-class ChangeStatusOfOrders < ActiveRecord::Migration[5.2]
-  def change
-    change_column_default :orders, :status, default: 0
-  end
-end
--- db/migrate/20211120101904_change_status_of_order_details.rb
+++
@@ -1,5 +0,0 @@
-class ChangeStatusOfOrderDetails < ActiveRecord::Migration[5.2]
-  def change
-    change_column_default :order_details, :status, default: 0
-  end
-end
