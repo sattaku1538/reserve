d6913a57428493e1d41c5a1f11c87899a695b4df
yu-M215 <yuu0609km.215@gmail.com>
2021-11-23 07:02:01 +0000

    Fix new customer registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name,:last_name,:first_name_ruby,:last_name_ruby,:post_code,:address,:telephone_number,:is_deleted])
+  end
 end
