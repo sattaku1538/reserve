f9de98ee43714f2a24331f5fcbca68208e68b871
sattaku1538 <choose.chose.chosen@gmail.com>
2021-11-23 02:10:12 +0000

    fix destroy

--- app/controllers/public/shippings_controller.rb
+++ app/controllers/public/shippings_controller.rb
@@ -1,6 +1,10 @@
 class Public::ShippingsController < ApplicationController
   before_action :set_shipping, only: %i[edit update destroy]
   
+  def set_shipping
+    @shipping = Shipping.find(params[:id])
+  end
+  
   def index
     @shippings = current_customer.shippings.all
     @shipping = Shipping.new
@@ -10,6 +14,7 @@ class Public::ShippingsController < ApplicationController
     @shipping = current_customer.shippings.new(shipping_params)
     if @shipping.save
       redirect_to public_shippings_path
+      flash[:notice] =  "配送先を登録しました。"
     else
       @shippings = current_customer.shippings.all
       render 'index'
@@ -29,16 +34,13 @@ class Public::ShippingsController < ApplicationController
   
   def destroy
     @shipping.destroy
-    @shipping = current_customer.shipping
+    # @shippings = current_customer.shippings
     flash[:notice] =  "配送先を削除しました。"
-    redirect_to public_shipping_path
+    redirect_to public_shippings_path
   end
   
   
   private
-  def set_shipping
-    @shipping = Shipping.find(params[:id])
-  end
   
   def shipping_params
     params.require(:shipping).permit(:post_code, :address, :address_name)
