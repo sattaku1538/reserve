af5629f9187884e94c3ac93c55cd42b252ac2f40
yu-M215 <yuu0609km.215@gmail.com>
2021-11-24 04:15:38 +0000

    Add before_action :set_category in public/products_controller

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,23 +1,28 @@
 class Public::ProductsController < ApplicationController
+
+  before_action :set_category
+
   def index
     @products = Product.where(is_sold: true).page(params[:page]).per(8)
-    @categories = Category.all
   end
 
   def show
     @product = Product.find(params[:id])
-    @categories = Category.all
     @cart_item = CartItem.new
   end
-  
+
   def search
     @keyword = params[:keyword]
     @products = Product.search_for(@keyword)
   end
 
   def search_category
-    @categories = Category.all
     @products = Product.where(category_id: params[:id]).page(params[:page]).per(8)
     @category_name = params[:name]
   end
+
+  def set_category
+    @categories = Category.all
+  end
+
 end
