5301239aae34fb409349a18c6bdd8acda85958bc
sattaku1538 <choose.chose.chosen@gmail.com>
2021-11-26 06:24:59 +0000

    Fix orders show total price

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -4,6 +4,10 @@ class OrderDetail < ApplicationRecord
 
   validates :product_id, :order_id, :quantity, :price, presence: true
 	validates :price, :quantity, numericality: { only_integer: true }
+	
+	def sum_of_price
+  price*quantity
+  end
 
 
   enum making_status: { default: 0, ready: 1, making: 2, finish: 3 }
